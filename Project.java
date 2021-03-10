c6291f9cd0773422d793a028b675eaea4ac3716f
Yateen Chiplunkar <yateenchiplunkar2010@gmail.com>
2021-03-10 13:29:32 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,35 @@
+package com.android.letsgetplaced.datamodel;
+
+import android.os.Parcel;
+
+
+
+public class Project extends ResumeEvent {
+
+    public Project() {
+    }
+
+    public static final Creator<Project> CREATOR = new Creator<Project>() {
+        @Override
+        public Project createFromParcel(Parcel in) {
+            return new Project(new ResumeEvent(in));
+        }
+
+        @Override
+        public Project[] newArray(int size) {
+            return new Project[size];
+        }
+    };
+
+    public Project(ResumeEvent event) {
+        super(event);
+    }
+
+    public String getName() {
+        return getTitle();
+    }
+
+    public void setName(String name) {
+        setTitle(name);
+    }
+}
